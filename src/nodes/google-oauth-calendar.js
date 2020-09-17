99558ff0d0bf5a70dd7fb04052c249b598b0f242
mdhom <max@dhom.info>
2020-09-17 20:54:45 +0200

    Implemented authorization UI and storing accesstoken in config node

--- src/nodes/google-oauth-calendar.js
+++ src/nodes/google-oauth-calendar.js
@@ -1,4 +1,5 @@
 module.exports = function(RED) {
+    const {google} = require('googleapis');
     
     function googleCredentials(config) {
         RED.nodes.createNode(this,config);
@@ -7,9 +8,10 @@ module.exports = function(RED) {
     
     RED.nodes.registerType("googleCredentials", googleCredentials, {
 		credentials: {
-			clientId:       { type:"text" },
-			clientSecret:   { type:"text" },
-			accessToken:    { type:"text" },
+			clientId:        { type:"text", required: true },
+            clientSecret:    { type:"text", required: true },
+            redirectUri:     { type:"text", required: true },
+            tokenData:       { type:"text" }
 		}
 	});
     
@@ -19,9 +21,99 @@ module.exports = function(RED) {
         var node = this;
 
         node.on('input', function(msg) {
-            node.send(msg);
+            try
+            {
+                if (googleCredentials !== null && googleCredentials !== undefined)
+                {
+                    if (googleCredentials.credentials.tokenData !== null && googleCredentials.credentials.tokenData !== undefined)
+                    {
+                        const oAuth2Client = new google.auth.OAuth2(
+                            googleCredentials.credentials.clientId, 
+                            googleCredentials.credentials.clientSecret, 
+                            googleCredentials.credentials.redirectUri);
+                
+                        oAuth2Client.setCredentials(JSON.parse(googleCredentials.credentials.tokenData));
+
+                        listUpcomingEvents(oAuth2Client, node, config.numEvents, function(err, result) {
+                            if (err) {
+                                node.status({fill:"red",shape:"dot",text:"Error: " + err});
+                            } else {
+                                msg.calendarConfig = googleCredentials.name;
+                                msg.payload = result.data.items;
+                                node.send(msg);
+                                node.status({fill:"green",shape:"dot",text:"Fetched " + result.data.items.length + " events"});
+                            }
+                        });
+                    }
+                    else 
+                    {
+                        node.status({fill:"red",shape:"dot",text:"Error: No AccessToken"});
+                    }
+                }
+                else
+                {
+                    node.status({fill:"red",shape:"dot",text:"Error: No Credentials"});
+                }
+            } catch (err) {
+                node.status({fill:"red",shape:"dot",text:"Exception: " + err});
+            }
         });
     }
 
     RED.nodes.registerType("googleApi",googleApi);
+
+    /**
+     * Lists the next 10 events on the user's primary calendar.
+     * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+     */
+    function listUpcomingEvents(auth, node, numEvents, callback) {
+        const calendar = google.calendar({version: 'v3', auth});
+            calendar.events.list({
+            calendarId: 'primary',
+            timeMin: (new Date()).toISOString(),
+            maxResults: numEvents,
+            singleEvents: true,
+            orderBy: 'startTime',
+            }, (err, res) => {
+                callback(err, res);
+            });
+    }
+  
+
+
+    RED.httpAdmin.get('/google-credentials/authUrl/:clientSecret/:clientId/:redirectUri', function(req, res){
+               
+        const client_secret = decodeURIComponent(req.params.clientSecret);
+        const client_id = decodeURIComponent(req.params.clientId);
+        const redirect_uri = decodeURIComponent(req.params.redirectUri);
+        const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uri);
+        const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+
+        const authUrl = oAuth2Client.generateAuthUrl({
+            access_type: 'offline',
+            scope: SCOPES,
+        });
+
+        res.end("{ \"authUrl\": \"" + authUrl + "\" }");
+    });
+
+    RED.httpAdmin.get('/google-credentials/token/:code/:nodeId/:clientSecret/:clientId/:redirectUri', function(req, res){
+        var node = RED.nodes.getNode(req.params.nodeId);
+
+        const client_secret = decodeURIComponent(req.params.clientSecret);
+        const client_id = decodeURIComponent(req.params.clientId);
+        const redirect_uri = decodeURIComponent(req.params.redirectUri);
+        const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uri);
+
+        oAuth2Client.getToken(decodeURIComponent(req.params.code), (err, token) => {
+          if (err){
+            res.send(500);
+            node.warn(err);
+          } else {
+            oAuth2Client.setCredentials(token);
+
+            res.end(JSON.stringify(token));
+          }
+        });
+    });
 }
